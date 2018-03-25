17ea17ae150ff784bfa55c403b600e78202df523
Gavin Nishizawa <ghn6069@rit.edu>
2018-03-25 17:12:52 -0400

    Fill null values in training data

--- src/read_data.py
+++ src/read_data.py
@@ -6,6 +6,18 @@ import os
 def main():
   td = pd.read_csv(os.path.join('input', 'train.csv'))
   print(td.head())
+  print("="*80)
+
+  # View fields with null values
+  print(td.isnull().sum())
+
+  # Fill nulls
+  # with age: median, cabin: 'UNKNOWN', Embarked: mode
+  td['Age'].fillna(td['Age'].median(), inplace=True)
+  td['Cabin'].fillna('UNKNOWN', inplace=True)
+  td['Embarked'].fillna(td['Embarked'].mode()[0], inplace=True)
+  
+  print(td.head())
   
 
 if __name__=='__main__':
